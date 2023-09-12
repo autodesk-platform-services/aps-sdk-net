dac1e88fc2c02ffae1f62e5f243b1a0324197444
Harun Gachanja Gitundu <arrotechdesign@gmail.com>
2023-09-12 16:53:37 +0300

    Setup GitHub Actions

--- datamanagement/test/TestDataManagement.cs
+++ datamanagement/test/TestDataManagement.cs
@@ -13,12 +13,12 @@ public class TestDataManagement
 {
     private static DataManagementClient _dataManagementApi = null!;
 
-    private const string token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjY0RE9XMnJoOE9tbjNpdk1NU0xlNGQ2VHEwUSIsInBpLmF0bSI6Ijd6M2gifQ.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6ImVXckR0dU54VnVHbHltd2w0VXdiZWJSVGx1bm13TzhnIiwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2Fqd3RleHA2MCIsImp0aSI6IjZHQ1JCSjRFYkdVNTQ3WmhSSjVtTVEzMEVSSlhFR2VYZUhpa0REWXhYb1d6S2kxZWRwdnZmM253ZDhBdjV4QW8iLCJleHAiOjE2OTQ1Mjc1MDh9.g-4WsVzYNVIZ9Z7FGFndg7eObAlOqwNKQuH-IFQiVejj0rJC8L40B5C1I7IdRunDVvGMRovq10sFvYH_fpQba4-pYLQy41X9hqhus9CBBvsYx63hHLpFV2J5AwJbCX-pc0JPwkCJNEhcBj0t1QJFd911B07b7UzFOJpGNYbNcvZUIxwzA3RVgbVwOQCbj-ahg_4K7z89XfXoW8l2nHqtwKpOfg2LVEqXg8EtE5KjfMugeAacgwZUAI8bE83aNcpuWUXpno0Gr_l2pd-0Wn_3D9McNL5-AxzP8jyhrUj3sSSd1vKT0WMJW7QAp05XOvRVPQ1MgREBVhVMPxAZXzviIw";
+    private const string token = "<token>";
 
     [ClassInitialize]
     public static void ClassInitialize(TestContext testContext)
     {
-        SDKManager sdkManager = SdkManagerBuilder
+        var sdkManager = SdkManagerBuilder
                 .Create() // Creates SDK Manager Builder itself.
                 .Add(new ApsConfiguration())
                 .Add(ResiliencyConfiguration.CreateDefault())
@@ -30,7 +30,7 @@ public class TestDataManagement
     [TestMethod]
     public async Task TestGetHubsAsync()
     {
-        Hubs getHubsResponse = await _webhooksApi.GetHubsAsync(accessToken: token);
+        Hubs getHubsResponse = await _dataManagementApi.GetHubsAsync(accessToken: token);
         Assert.IsTrue(getHubsResponse.Data.Count > 0);
     }
 
--- webhooks/test/TestWebhooks.cs
+++ webhooks/test/TestWebhooks.cs
@@ -13,13 +13,13 @@ public class TestWebhooks
 {
     private static WebhooksClient _webhooksApi = null!;
 
-    private const string token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjY0RE9XMnJoOE9tbjNpdk1NU0xlNGQ2VHEwUSIsInBpLmF0bSI6Ijd6M2gifQ.eyJzY29wZSI6WyJjb2RlOmFsbCIsImRhdGE6d3JpdGUiLCJkYXRhOnJlYWQiLCJidWNrZXQ6Y3JlYXRlIiwiYnVja2V0OmRlbGV0ZSIsImJ1Y2tldDpyZWFkIl0sImNsaWVudF9pZCI6ImVXckR0dU54VnVHbHltd2w0VXdiZWJSVGx1bm13TzhnIiwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2Fqd3RleHA2MCIsImp0aSI6IjZHQ1JCSjRFYkdVNTQ3WmhSSjVtTVEzMEVSSlhFR2VYZUhpa0REWXhYb1d6S2kxZWRwdnZmM253ZDhBdjV4QW8iLCJleHAiOjE2OTQ1Mjc1MDh9.g-4WsVzYNVIZ9Z7FGFndg7eObAlOqwNKQuH-IFQiVejj0rJC8L40B5C1I7IdRunDVvGMRovq10sFvYH_fpQba4-pYLQy41X9hqhus9CBBvsYx63hHLpFV2J5AwJbCX-pc0JPwkCJNEhcBj0t1QJFd911B07b7UzFOJpGNYbNcvZUIxwzA3RVgbVwOQCbj-ahg_4K7z89XfXoW8l2nHqtwKpOfg2LVEqXg8EtE5KjfMugeAacgwZUAI8bE83aNcpuWUXpno0Gr_l2pd-0Wn_3D9McNL5-AxzP8jyhrUj3sSSd1vKT0WMJW7QAp05XOvRVPQ1MgREBVhVMPxAZXzviIw";
+    private const string token = "<token>";
 
     [ClassInitialize]
     public static void ClassInitialize(TestContext testContext)
     {
         var sdkManager = SdkManagerBuilder
-                .Create() // Creates SDK Manager Builder itself.
+                .Create()
                 .Add(new ApsConfiguration())
                 .Add(ResiliencyConfiguration.CreateDefault())
                 .Build();
@@ -34,18 +34,18 @@ public class TestWebhooks
         Assert.IsTrue(getHooksResponse.Data.Count > 0);
     }
 
-    // [TestMethod]
-    // public async Task TestCreateHookAsync()
-    // {
-    //     HookPayload createSpecifiedEventHook = new HookPayload();
-    //     createSpecifiedEventHook.CallbackUrl = "<CallbackUrl>";
-    //     createSpecifiedEventHook.Scope = createSpecifiedEventHook.Scope.SetScope(Scopes.Workflow, "<my-workflow-id-new-test-nine>");
+    [TestMethod]
+    public async Task TestCreateHookAsync()
+    {
+        HookPayload createSpecifiedEventHook = new HookPayload();
+        createSpecifiedEventHook.CallbackUrl = "<CallbackUrl>";
+        createSpecifiedEventHook.Scope = createSpecifiedEventHook.Scope.SetScope(Scopes.Workflow, "<my-workflow-id-new-test-nine>");
 
-    //     HttpResponseMessage createSpecifiedEventHookResponse = await _webhooksApi.CreateSystemEventHookAsync(system: Systems.Derivative, _event: Events.ExtractionFinished, hookPayload: createSpecifiedEventHook, accessToken: token);
-    //     var statusCode = createSpecifiedEventHookResponse.StatusCode;
-    //     string statusCodeString = statusCode.ToString();
-    //     Assert.IsTrue(statusCodeString == "Created");
-    // }
+        HttpResponseMessage createSpecifiedEventHookResponse = await _webhooksApi.CreateSystemEventHookAsync(system: Systems.Derivative, _event: Events.ExtractionFinished, hookPayload: createSpecifiedEventHook, accessToken: token);
+        var statusCode = createSpecifiedEventHookResponse.StatusCode;
+        string statusCodeString = statusCode.ToString();
+        Assert.IsTrue(statusCodeString == "Created");
+    }
 
 
 }
