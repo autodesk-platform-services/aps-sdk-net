18dcdd923e369b59fd09a10319d69a66459e0e8b
srivastavarahull <139200493+srivastavarahull@users.noreply.github.com>
2024-06-28 15:09:00 +0530

    Updated Issues Samples

--- samples/issues.cs
+++ samples/issues.cs
@@ -8,7 +8,9 @@ namespace Samples
     class Issues
     {
         string token = "<token>";
-        string projectid = "<project_id>";
+        string project_id = "<project_id>";
+        string issue_id = "<issueId>";
+
         IssuesClient issuesClient = null!;
 
         public void Initialise()
@@ -25,27 +27,26 @@ namespace Samples
         // Get user Permission
         public async Task getUserInfo()
         {
-            User userProfile = await issuesClient.GetUserProfileAsync(projectid, xAdsRegion: XAdsRegion.US, accessToken: token);
+            User userProfile = await issuesClient.GetUserProfileAsync(projectId:project_id, xAdsRegion: Region.US, accessToken: token);
         }
 
 
         // Get IssueType
         public async Task getIssueType()
         {
-            IssueType Type = await issuesClient.GetIssuesTypesAsync(projectid, xAdsRegion: XAdsRegion.US, accessToken: token);
+            IssueType Type = await issuesClient.GetIssuesTypesAsync(projectId:project_id, xAdsRegion: Region.US, accessToken: token);
         }
 
         //Get Issues
         public async Task getIssues()
         {
-            IssuesPage issues = await issuesClient.GetIssuesAsync(projectid, xAdsRegion: XAdsRegion.US, accessToken: token);
+            IssuesPage issues = await issuesClient.GetIssuesAsync(projectId:project_id, xAdsRegion: Region.US, accessToken: token);
         }
 
         //Get details of a issue
         public async Task getIssueDetail()
         {
-            string issueId = "d59ba4ba-8070-474c-a0a0-adad106ae1a5";
-            Issue issuedetail = await issuesClient.GetIssueDetailsAsync(projectid, issueId, xAdsRegion: XAdsRegion.US, accessToken: token);
+            Issue issuedetail = await issuesClient.GetIssueDetailsAsync(projectId:project_id, issueId:issue_id, xAdsRegion: Region.US, accessToken: token);
         }
 
         //Create Issue 
@@ -55,33 +56,31 @@ namespace Samples
             newIssue.Title = "Issue Created By using SDK ";
             newIssue.Description = "Created for test";
             newIssue.Status = Status.Open;
-            newIssue.AssignedTo = "J2Q2EL4FKPKRHTKF";
+            newIssue.AssignedTo = "<AssignedTo>";
             newIssue.AssignedToType = AssignedToType.User;
-            newIssue.IssueSubtypeId = "9f39edab-8773-440d-848b-99d098e86ce3";
+            newIssue.IssueSubtypeId = "<IssueSubtypeId>";
             newIssue.DueDate = "2023-10-10";
-            Issue createissue = await issuesClient.CreateIssueAsync(projectid, xAdsRegion: XAdsRegion.US, newIssue, accessToken: token);
+            Issue createissue = await issuesClient.CreateIssueAsync(projectId:project_id, xAdsRegion: Region.US, newIssue, accessToken: token);
         }
 
         // Create Comment
         public async Task createComment()
         {
-            string issueId = "d59ba4ba-8070-474c-a0a0-adad106ae1a5";
             CommentsPayload newcomment = new CommentsPayload();
             newcomment.Body = "Created a Comment for testing SDK";
-            CreatedComment createComment = await issuesClient.CreateCommentsAsync(projectid, issueId, xAdsRegion: XAdsRegion.US, commentsPayload: newcomment, accessToken: token);
+            CreatedComment createComment = await issuesClient.CreateCommentsAsync(projectId:project_id, issueId:issue_id, xAdsRegion: Region.US, commentsPayload: newcomment, accessToken: token);
         }
 
         //Get Comments
         public async Task getComments()
         {
-            string issueId = "d59ba4ba-8070-474c-a0a0-adad106ae1a5";
-            Comments getComments = await issuesClient.GetCommentsAsync(projectid, issueId, xAdsRegion: XAdsRegion.US, accessToken: token);
+            Comments getComments = await issuesClient.GetCommentsAsync(projectId:project_id, issueId:issue_id, xAdsRegion: Region.US, accessToken: token);
             Console.WriteLine(getComments);
         }
 
         public async Task getAttrdefinition()
         {
-            AttrDefinition attrDefinition = await issuesClient.GetAttributeDefinitionsAsync(projectid, xAdsRegion: XAdsRegion.US, accessToken: token);
+            AttrDefinition attrDefinition = await issuesClient.GetAttributeDefinitionsAsync(projectId:project_id, xAdsRegion: Region.US, accessToken: token);
             Console.WriteLine(attrDefinition);
         }
 
