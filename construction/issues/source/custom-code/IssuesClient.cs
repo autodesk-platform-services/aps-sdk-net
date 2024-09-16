3ec732e46bac030932bac87efdb336292e37f796
srivastavarahull <139200493+srivastavarahull@users.noreply.github.com>
2024-09-16 17:57:20 +0530

    Made Accesstoken & Region to default and removing null

--- construction/issues/source/custom-code/IssuesClient.cs
+++ construction/issues/source/custom-code/IssuesClient.cs
@@ -45,7 +45,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId"></param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="limit">The number of custom attribute definitions to return in the response payload. For example, limit&#x3D;2. Acceptable values: 1-200. Default value: 200. (optional)</param>/// <param name="offset">The number of custom attribute definitions you want to begin retrieving results from. (optional)</param>/// <param name="filterCreatedAt">Retrieves items that were created at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterUpdatedAt">Retrieves items that were last updated at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterDeletedAt">Retrieves types that were deleted at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas.  (optional)</param>/// <param name="filterDataType">Retrieves issue custom attribute definitions with the specified data type. Possible values: list (this corresponds to dropdown in the UI), text, paragraph, numeric. For example, filter[dataType]&#x3D;text,numeric. (optional)</param>
                 /// <returns>Task of ApiResponse<AttrDefinition></returns>
 
-                public async System.Threading.Tasks.Task<AttrDefinition> GetAttributeDefinitionsAsync(string projectId, Region xAdsRegion = default, int? limit = default(int?), int? offset = default(int?), string filterCreatedAt = default(string), string filterUpdatedAt = default(string), string filterDeletedAt = default(string), List<DataType> filterDataType = default(List<DataType>), string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<AttrDefinition> GetAttributeDefinitionsAsync(string projectId, Region xAdsRegion = default, int? limit = default(int?), int? offset = default(int?), string filterCreatedAt = default(string), string filterUpdatedAt = default(string), string filterDeletedAt = default(string), List<DataType> filterDataType = default(List<DataType>), string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -70,7 +70,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="limit">The number of custom attribute mappings to return in the response payload. For example, limit&#x3D;2. Acceptable values: 1-200. Default value: 200. (optional)</param>/// <param name="offset">The number of custom attribute mappings you want to begin retrieving results from. (optional)</param>/// <param name="filterCreatedAt">Retrieves items that were created at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterUpdatedAt">Retrieves items that were last updated at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterDeletedAt">Retrieves types that were deleted at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterAttributeDefinitionId">Retrieves issue custom attribute mappings associated with the specified issue custom attribute definitions. Separate multiple values with commas. For example: filter[attributeDefinitionId]&#x3D;18ee5858-cbf1-451a-a525-7c6ff8156775. (optional)</param>/// <param name="filterMappedItemId">Retrieves issue custom attribute mappings associated with the specified items (project, type, or subtype). Separate multiple values with commas. For example: filter[mappedItemId]&#x3D;18ee5858-cbf1-451a-a525-7c6ff8156775. Note that this does not retrieve inherited custom attribute mappings or custom attribute mappings of descendants. (optional)</param>
                 /// <returns>Task of ApiResponse<AttrMapping></returns>
 
-                public async System.Threading.Tasks.Task<AttrMapping> GetAttributeMappingsAsync(string projectId, Region xAdsRegion = default, int? limit = default(int?), int? offset = default(int?), string filterCreatedAt = default(string), string filterUpdatedAt = default(string), string filterDeletedAt = default(string), string filterAttributeDefinitionId = default(string), string filterMappedItemId = default(string), string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<AttrMapping> GetAttributeMappingsAsync(string projectId, Region xAdsRegion = default, int? limit = default(int?), int? offset = default(int?), string filterCreatedAt = default(string), string filterUpdatedAt = default(string), string filterDeletedAt = default(string), string filterAttributeDefinitionId = default(string), string filterMappedItemId = default(string), string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -93,7 +93,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="issueId">The unique identifier of the issue.</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="contentType">Must be application/json</param>/// <param name="body"> (optional)</param>
                 /// <returns>Task of ApiResponse<CreatedComment></returns>
 
-                public async System.Threading.Tasks.Task<CreatedComment> CreateCommentsAsync(string projectId, string issueId, CommentsPayload commentsPayload, Region xAdsRegion = default, string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<CreatedComment> CreateCommentsAsync(string projectId, string issueId, CommentsPayload commentsPayload, Region xAdsRegion = default, string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -116,7 +116,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="issueId">The unique identifier of the issue.</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="limit">Add limit&#x3D;20 to limit the results count (together with the offset to support pagination). (optional)</param>/// <param name="offset">Add offset&#x3D;20 to get partial results (together with the limit to support pagination). (optional)</param>/// <param name="sortBy">Sort issue comments by specified fields. Separate multiple values with commas. To sort in descending order add a - (minus sign) before the sort criteria (optional)</param>
                 /// <returns>Task of ApiResponse<Comments></returns>
 
-                public async System.Threading.Tasks.Task<Comments> GetCommentsAsync(string projectId, string issueId, Region xAdsRegion = default, string limit = default(string), string offset = default(string), List<SortBy> sortBy = default(List<SortBy>), string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<Comments> GetCommentsAsync(string projectId, string issueId, Region xAdsRegion = default, string limit = default(string), string offset = default(string), List<SortBy> sortBy = default(List<SortBy>), string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -140,7 +140,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="include">Add ‘include&#x3D;rootcauses’ to add the root causes for each category. (optional)</param>/// <param name="limit">Add limit&#x3D;20 to limit the results count (together with the offset to support pagination). (optional)</param>/// <param name="offset">Add offset&#x3D;20 to get partial results (together with the limit to support pagination) (optional)</param>/// <param name="filterUpdatedAt">Retrieves root cause categories updated at the specified date and time, in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>
                 /// <returns>Task of ApiResponse<IssueRootCause></returns>
 
-                public async System.Threading.Tasks.Task<IssueRootCause> GetRootCauseCategoriesAsync(string projectId, Region xAdsRegion = default, string include = default(string), int? limit = default(int?), int? offset = default(int?), string filterUpdatedAt = default(string), string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<IssueRootCause> GetRootCauseCategoriesAsync(string projectId, Region xAdsRegion = default, string include = default(string), int? limit = default(int?), int? offset = default(int?), string filterUpdatedAt = default(string), string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -164,7 +164,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="contentType">Must be application/json</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="issueUpdateRequest"> (optional)</param>
                 /// <returns>Task of ApiResponse<IssueUpdateResponse></returns>
 
-                public async System.Threading.Tasks.Task<Issue> CreateIssueAsync(string projectId, IssuePayload issuePayload, Region xAdsRegion = default, string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<Issue> CreateIssueAsync(string projectId, IssuePayload issuePayload, Region xAdsRegion = default, string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -187,7 +187,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId">The ID of the project.</param>/// <param name="issueId">The unique identifier of the issue.</param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>
                 /// <returns>Task of ApiResponse<Issue></returns>
 
-                public async System.Threading.Tasks.Task<Issue> GetIssueDetailsAsync(string projectId, string issueId, Region xAdsRegion = default, string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<Issue> GetIssueDetailsAsync(string projectId, string issueId, Region xAdsRegion = default, string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -258,7 +258,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId"></param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>
                 /// <returns>Task of ApiResponse<User></returns>
 
-                public async System.Threading.Tasks.Task<User> GetUserProfileAsync(string projectId, Region xAdsRegion = default, string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<User> GetUserProfileAsync(string projectId, Region xAdsRegion = default, string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
@@ -282,7 +282,7 @@ namespace Autodesk.Construction.Issues
                 /// <param name="projectId"></param>/// <param name="xAdsRegion">The region where the bucket residesAcceptable values: &#x60;US&#x60;, &#x60;EMEA&#x60;</param>/// <param name="include">Use include&#x3D;subtypes to include the types (subtypes) for each category (type). (optional)</param>/// <param name="limit">Add limit&#x3D;20 to limit the results count (together with the offset to support pagination). (optional)</param>/// <param name="offset">Add offset&#x3D;20 to get partial results (together with the limit to support pagination). (optional)</param>/// <param name="filterUpdatedAt">Retrieves types that were last updated at the specified date and time, in in one of the following URL-encoded formats: YYYY-MM-DDThh:mm:ss.sz or YYYY-MM-DD. Separate multiple values with commas. (optional)</param>/// <param name="filterIsActive">Filter types by status e.g. filter[isActive]&#x3D;true will only return active types. Default value: undefined (meaning both active &amp; inactive issue type categories will return). (optional)</param>
                 /// <returns>Task of ApiResponse<IssueType></returns>
 
-                public async System.Threading.Tasks.Task<IssueType> GetIssuesTypesAsync(string projectId, string include = default(string), int? limit = default(int?), int? offset = default(int?), string filterUpdatedAt = default(string), bool? filterIsActive = default(bool?), Region xAdsRegion = default, string accessToken = null, bool throwOnError = true)
+                public async System.Threading.Tasks.Task<IssueType> GetIssuesTypesAsync(string projectId, string include = default(string), int? limit = default(int?), int? offset = default(int?), string filterUpdatedAt = default(string), bool? filterIsActive = default(bool?), Region xAdsRegion = default, string accessToken = default, bool throwOnError = true)
                 {
                         if (String.IsNullOrEmpty(accessToken) && this.AuthenticationProvider == null)
                         {
