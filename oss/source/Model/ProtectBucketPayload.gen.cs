137e965875538a7376c272f1a4d54f744f3c90d4
Tyler Warner <twarner@rand.com>
2026-05-08 06:17:43 -0500

    Add ProtectBucketAsync() implementation & test to OSS client.

---
+++ oss/source/Model/ProtectBucketPayload.gen.cs
@@ -0,0 +1,74 @@
+/* 
+ * APS SDK
+ *
+ * The APS Platform contains an expanding collection of web service components that can be used with Autodesk cloud-based products or your own technologies. Take advantage of Autodesk’s expertise in design and engineering.
+ *
+ * oss
+ *
+ * The Object Storage Service (OSS) allows your application to download and upload raw files (such as PDF, XLS, DWG, or RVT) that are managed by the Data service.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Autodesk.Oss.Model
+{
+    /// <summary>
+    /// The request payload for the Protect Bucket operation.
+    /// </summary>
+    [DataContract]
+    public partial class ProtectBucketPayload
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProtectBucketPayload" /> class.
+        /// </summary>
+        public ProtectBucketPayload()
+        { }
+
+        /// <summary>
+        /// Indicates the protection status of the bucket. Possible values are:
+        /// 
+        /// - `true` - Protects the bucket from accidental deletion.
+        /// - `false` - Leaves the bucket unprotected or removes protection from a previously protected bucket.
+        /// </summary>
+        /// <value>
+        /// Indicates the protection status of the bucket. Possible values are:
+        /// 
+        /// - `true` - Protects the bucket from accidental deletion.
+        /// - `false` - Leaves the bucket unprotected or removes protection from a previously protected bucket.
+        /// </value>
+        [DataMember(Name = "protection", EmitDefaultValue = true)]
+        public bool Protection { get; set; }
+
+        /// <summary>
+        /// Returns the string presentation of the object.
+        /// </summary>
+        /// <returns>
+        /// String presentation of the object.
+        /// </returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
